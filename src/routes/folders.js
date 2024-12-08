d565f50842e06b66d3941354cc1fef9b2b156307
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-12-08 23:41:02 +0700

    feat: add middleware for file upload validation

--- src/routes/folders.js
+++ src/routes/folders.js
@@ -4,13 +4,13 @@ const router = express.Router({ mergeParams: true });
 const folders = require('../controllers/folders');
 const catchAsync = require('../utils/catchAsync');
 const { authenticate } = require('../middleware/auth');
-const upload = require('../middleware/multer');
+const {upload, handleMulterError } = require('../middleware/multer');
 
 
 router.route('/create').post(authenticate, catchAsync(folders.createFolder))
 router.route('/getListFolder').get(authenticate, catchAsync(folders.getFolders));
 
-router.post('/:folderId/upload', authenticate, upload.single('file'), catchAsync(folders.uploadDocument));
+router.post('/:folderId/upload', authenticate, upload.single('file'), handleMulterError, catchAsync(folders.uploadDocument));
 
 router.route('/:folderId/update').put(authenticate, catchAsync(folders.updateFolder))
 router.route('/:folderId/delete').delete(authenticate, catchAsync(folders.deleteFolder));
